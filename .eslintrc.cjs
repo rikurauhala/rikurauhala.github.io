32ae68a2c58bd6f9aa4369c062f71e7d77dae51e
Riku Rauhala <riku@rauhala.io>
2024-06-29 16:35:39 +0300

    Update ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,33 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['import', 'react', 'react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'import/order': [
+      'error',
+      {
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling'],
+        pathGroups: [{ pattern: '~/**', group: 'internal', position: 'before' }],
+      },
     ],
+    'no-constant-condition': 'off',
+    'react/jsx-sort-props': ['error', { reservedFirst: true }],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+  settings: {
+    'import/resolver': {
+      'eslint-import-resolver-custom-alias': {
+        alias: {
+          '~': './src',
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
 }

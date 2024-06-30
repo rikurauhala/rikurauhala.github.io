66f9adaee4e1c8c8d552cbf441715825b3e3a2f3
Riku Rauhala <riku@rauhala.io>
2024-06-30 17:23:22 +0300

    Actually add profile picture

--- src/components/InfoCard/ProfilePicture.tsx
+++ src/components/InfoCard/ProfilePicture.tsx
@@ -3,7 +3,15 @@ import Avatar from '@mui/joy/Avatar'
 const ProfilePicture = (): JSX.Element => {
   const size = '150px'
 
-  return <Avatar sx={{ height: size, width: size }} />
+  return (
+    <Avatar
+      alt="Profile picture"
+      src="/img/profile.png"
+      sx={{ height: size, width: size }}
+      title="Profile picture"
+      variant="outlined"
+    />
+  )
 }
 
 export default ProfilePicture

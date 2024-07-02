d4cf7d25cac1fa3f8c89668fb5ceb64371252fae
Riku Rauhala <riku@rauhala.io>
2024-07-02 22:31:57 +0300

    Extract about section content

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,6 @@
 import Card from '@mui/joy/Card'
 import Typography from '@mui/joy/Typography'
+import { about } from '~/content/about'
 
 const About = (): JSX.Element => {
   return (
@@ -7,22 +8,11 @@ const About = (): JSX.Element => {
       <Typography component="h2" level="title-lg">
         About
       </Typography>
-      <Typography component="p" level="body-md">
-        Hi there! I am a full stack software developer with two years of professional experience in
-        the IT field, one of which writing code. I am currently working as a software developer
-        while obtaining my master's degree in computer science.
-      </Typography>
-      <Typography component="p" level="body-md">
-        I enjoy working on various long-term projects in collaboration with other people. My
-        strongest skills include problem solving and teamwork. As a software engineer, I embrace
-        continuous learning. I am willing to learn new technologies and master new tools to grow as
-        a professional.
-      </Typography>
-      <Typography component="p" level="body-md">
-        My professional and academic interests include software engineering, cyber security,
-        cryptography and human-computer interaction. It is essential to build software that is both
-        secure and user-friendly.
-      </Typography>
+      {about.split('\n\n').map((line, index) => (
+        <Typography key={index} component="p" level="body-md">
+          {line}
+        </Typography>
+      ))}
     </Card>
   )
 }
---
+++ src/content/about.ts
@@ -0,0 +1,14 @@
+export const about = `
+  Hi there! I am a full stack software developer with two years of professional experience in
+  the IT field, one of which writing code. I am currently working as a software developer
+  while obtaining my master's degree in computer science.
+
+  I enjoy working on various long-term projects in collaboration with other people. My
+  strongest skills include problem solving and teamwork. As a software engineer, I embrace
+  continuous learning. I am willing to learn new technologies and master new tools to grow as
+  a professional.
+
+  My professional and academic interests include software engineering, cyber security,
+  cryptography and human-computer interaction. It is essential to build software that is both
+  secure and user-friendly.
+`

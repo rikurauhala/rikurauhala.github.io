50f9138482f9079212d2090e8ea9f33e65abd826
Riku Rauhala <riku@rauhala.io>
2024-07-13 21:00:05 +0300

    Make the SearchBar more functional

--- src/components/Projects/SearchBar.tsx
+++ src/components/Projects/SearchBar.tsx
@@ -1,8 +1,33 @@
+import ClearIcon from '@mui/icons-material/Clear'
 import SearchIcon from '@mui/icons-material/Search'
+import IconButton from '@mui/joy/IconButton'
 import Input from '@mui/joy/Input'
+import { useState } from 'react'
 
 const SearchBar = (): JSX.Element => {
-  return <Input fullWidth placeholder="Search" startDecorator={<SearchIcon />} size="lg" />
+  const [search, setSearch] = useState('')
+
+  const handleClear = () => {
+    setSearch('')
+  }
+
+  return (
+    <Input
+      endDecorator={
+        search && (
+          <IconButton onClick={handleClear} sx={{ marginRight: -1 }}>
+            <ClearIcon />
+          </IconButton>
+        )
+      }
+      fullWidth
+      onChange={(event) => setSearch(event.target.value)}
+      placeholder="Search"
+      size="lg"
+      startDecorator={<SearchIcon />}
+      value={search}
+    />
+  )
 }
 
 export default SearchBar

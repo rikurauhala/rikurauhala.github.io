03dc042921f5bde1d227c9a3f7b3bba1a3c2d2d1
Riku Rauhala <riku@rauhala.io>
2025-03-24 11:43:41 +0200

    Update about section content

--- src/content/about.ts
+++ src/content/about.ts
@@ -1,7 +1,6 @@
 export const about = `
-  Hi there! I am a full stack software developer with two years of professional experience in
-  the IT field, one of which writing code. I am currently working as a software developer
-  while obtaining my master's degree in computer science.
+  Hi there! I am a full stack software developer with three years of professional experience in
+  the IT field, two of which writing code. I am currently writing my master's thesis.
 
   I enjoy working on various long-term projects in collaboration with other people. My
   strongest skills include problem solving and teamwork. As a software engineer, I embrace
